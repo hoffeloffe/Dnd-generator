2822aef0a8bfbef2b3ae533daf5322123a7ee124
xTaig4 <73821893+xTaig4@users.noreply.github.com>
2022-02-18 10:48:41 +0100

    controlled components

--- src/NPCMaker.js
+++ src/NPCMaker.js
@@ -1,79 +1,136 @@
 //import { getValue } from "@testing-library/user-event/dist/utils";
 import React, { useState, useEffect } from "react";
 import "./GameBrowser.css"
-import { playerNames, playerRace, playerClass, playerFeats, playerBackground  } from "./name.js";
+import { playerNames, playerRace, playerClass, playerFeats, playerBackground } from "./name.js";
 
 class PlayerList extends React.Component {
-    // constructor() {
-    //   super(); duper test
-    // }
+    constructor() {
+        super();
+        this.state = {
+            name: "",
+            race: "",
+            class: ""
+        };
+        this.onChange = this.onChange.bind(this);
+        this.createCharecter = this.createCharecter.bind(this);
+    }
 
-    render()
-    {
+    createCharecter() {    
+        console.log("testing")
+            return (
+                <>
+                <div id="CharacterTemplate" class="quest">
+                    <button class="buttonRemove"><small>x</small></button> <label>Character Info</label>
+                </div>
+                </>
+            )
+    }
+
+    onChange(event) {
+        let inputName = event.target.name;
+        let inputValue = event.target.value;
+        this.setState({ [inputName]: inputValue })
+    }
+
+    render() {
         return (
             <>
+                <CharacterCreation onChange={this.onChange} createcharacter={this.createCharecter} {...this.state} />
+                <CharacterPreview {...this.state} />
+
+
                 <WatchWithHooks />
                 <DisplayStats />
-           </>
+            </>
         );
     }
-  }
-
-  let characterContainerElement = document.getElementById("CharacterContainer");
-
-  let inputName = document.getElementById('inputname');
-  let inputRace = document.getElementById("inputRace");
-  let inputClass = document.getElementById("inputClass");
-
-  let charTemp = document.getElementById("CharacterTemplate");
-  let creatbtn = document.getElementById("btnCreate");
-  let rancrtbtn = document.getElementById("rancrtbtn");
+}
 
 
-function randomPropNumber(props)
-{
-    var selected = Math.floor(Math.random()*props.length);
+let characterContainerElement = document.getElementById("CharacterContainer");
+
+let inputName = document.getElementById('inputname');
+let inputRace = document.getElementById("inputRace");
+let inputClass = document.getElementById("inputClass");
+
+let charTemp = document.getElementById("CharacterTemplate");
+let creatbtn = document.getElementById("btnCreate");
+let rancrtbtn = document.getElementById("rancrtbtn");
+
+function CharacterCreation(props) {
+    return (
+        <>
+            <br />
+            <label>Name:</label>
+            <br />
+            <input onChange={props.onChange} name="name" value={props.name} ></input>
+            <br />
+            <select onChange={props.onChange} name="race" value={props.race}>
+                <option>Elf</option>
+                <option>Dragonborn</option>
+            </select>
+            <br />
+            <select onChange={props.onChange} name="class" value={props.class}>
+                <option>Mage</option>
+                <option>Rouge</option>
+            </select><br/>
+            <button createcharacter={props.createCharecter} id="create">Create Character</button>
+        </>
+    )
+}
+
+function CharacterPreview(props) {
+    return (
+        <>
+            <h3>Preview:</h3>
+            <div>Name: {props.name}</div>
+            <div>Race: {props.race}</div>
+            <div>Class: {props.class}</div>
+
+        </>
+    )
+}
+
+
+
+function randomPropNumber(props) {
+    var selected = Math.floor(Math.random() * props.length);
     return selected;
 }
 
-rancrtbtn.onclick = function()
-{
+rancrtbtn.onclick = function () {
     createCharector(
         playerNames[randomPropNumber(playerNames)],
         playerRace[randomPropNumber(playerRace)],
         playerClass[randomPropNumber(playerClass)],
         DisplayStats(),
         playerFeats[randomPropNumber(playerFeats)],
-        Math.floor(Math.random()*4 + 1),
+        Math.floor(Math.random() * 4 + 1),
         playerBackground[randomPropNumber(playerBackground)],
-        );
+    );
 }
 
-creatbtn.onclick = function()
-{
+creatbtn.onclick = function () {
     createCharector(inputName.value, inputRace.value, inputClass.value, DisplayStats(),
-    playerFeats[randomPropNumber(playerFeats)],
-    Math.floor(Math.random()*4 + 1),
-    playerBackground[randomPropNumber(playerBackground)]);
+        playerFeats[randomPropNumber(playerFeats)],
+        Math.floor(Math.random() * 4 + 1),
+        playerBackground[randomPropNumber(playerBackground)]);
 }
 
-function createCharector(Name, Race, Dndclass, PlayerStats, Feats, Currentlv, PlayerBackground)
-{
-    if(Name === '')
-    Name = "Hoffe";
-    if(Race === '')
-    Race = "Human";
-    if(Dndclass === '')
-    Dndclass = "Programer";
+function createCharector(Name, Race, Dndclass, PlayerStats, Feats, Currentlv, PlayerBackground) {
+    if (Name === '')
+        Name = "Hoffe";
+    if (Race === '')
+        Race = "Human";
+    if (Dndclass === '')
+        Dndclass = "Programer";
 
     let supclass;
 
-    if(Currentlv >= 3)
-    {
+    if (Currentlv >= 3) {
         supclass = " (" + Dndclass.sub[randomPropNumber(Dndclass.sub)] + ")";
     }
-    else
-    {
+    else {
         supclass = '';
     }
 
@@ -89,16 +146,16 @@ function createCharector(Name, Race, Dndclass, PlayerStats, Feats, Currentlv, Pl
     raceName.innerText = "Race: " + Race;
 
     let dndclass = clonedCharacter.getElementsByClassName("dndclass")[0];
-    dndclass.innerText = "Class: " +  Dndclass.name + supclass;
+    dndclass.innerText = "Class: " + Dndclass.name + supclass;
 
     let feats = clonedCharacter.getElementsByClassName("feats")[0];
-    feats.innerText = "Feats: " +  Feats.name;
+    feats.innerText = "Feats: " + Feats.name;
 
     let currentlv = clonedCharacter.getElementsByClassName("currentlv")[0];
-    currentlv.innerText = "Lv: " +  Currentlv;
+    currentlv.innerText = "Lv: " + Currentlv;
 
     let playerBackground = clonedCharacter.getElementsByClassName("background")[0];
-    playerBackground.innerText = "Back: " +  PlayerBackground.name;
+    playerBackground.innerText = "Back: " + PlayerBackground.name;
 
     /////
     // STATS
@@ -176,8 +233,7 @@ function createCharector(Name, Race, Dndclass, PlayerStats, Feats, Currentlv, Pl
 
     let clonedButton = clonedCharacter.getElementsByTagName("button")[0];
 
-    clonedButton.onclick = function()
-    {
+    clonedButton.onclick = function () {
         this.parentNode.remove();
     };
 
@@ -189,13 +245,12 @@ function createCharector(Name, Race, Dndclass, PlayerStats, Feats, Currentlv, Pl
 }
 
 // Calculate the skill level
-function handleProficien(skill, prof, currentlv, stat)
-{
+function handleProficien(skill, prof, currentlv, stat) {
     // If the skill is 10, bonus is 0. Otherwise it changes on every even it goes up or uneven down.
     var result = Math.floor((stat - 10) / 2);
 
-    if(prof.includes(skill)){
-        var profBonus = Math.floor(2 + (currentlv-1)/4);
+    if (prof.includes(skill)) {
+        var profBonus = Math.floor(2 + (currentlv - 1) / 4);
         result += profBonus;
     }
     return result
@@ -203,35 +258,31 @@ function handleProficien(skill, prof, currentlv, stat)
 
 
 
-function StandardArray(props)
-{
-    var selected = Math.floor(Math.random()*props.length);
+function StandardArray(props) {
+    var selected = Math.floor(Math.random() * props.length);
     var stat = props[selected]
     props.splice(selected, 1);
     return stat
 }
 
-function WatchWithHooks()
-{
+function WatchWithHooks() {
     let date = useDate();
 
-    return(<h1>{date.toLocaleTimeString()}</h1>)
+    return (<h1>{date.toLocaleTimeString()}</h1>)
 }
 
-function useDate()
-{
+function useDate() {
     const [date, setDate] = useState(new Date());
 
     useEffect(() => {
-        let id = setInterval(() => setDate(new Date()),100);
+        let id = setInterval(() => setDate(new Date()), 100);
         return () => clearInterval(id);
     })
 
     return date;
 }
 
-function DisplayStats()
-{
+function DisplayStats() {
     var stats = [15, 14, 13, 12, 10, 8];
 
     var result = [StandardArray(stats), StandardArray(stats), StandardArray(stats), StandardArray(stats), StandardArray(stats), StandardArray(stats)];
